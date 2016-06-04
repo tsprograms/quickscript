3d026341538405d4939a73a78f61554080c5f2a2
tsprograms <tsprograms@users.noreply.github.com>
2016-06-03 20:43:05 -0400

    Create input-stream.js

---
+++ input-stream.js
@@ -0,0 +1,5 @@
+/*
+This file creates a simple input stream interface, exported to the root context as InputStream (or as module.exports).
+Created on 6/3/16 by TSPrograms.
+Copyright © 2016 TSPrograms.
+*/
