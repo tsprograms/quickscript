f78bf8f4911681a43664619e45302d7a79cb732b
tsprograms <tsprograms@users.noreply.github.com>
2016-06-03 20:50:37 -0400

    Add basic constructor to input-stream.js

--- input-stream.js
+++ input-stream.js
@@ -3,3 +3,37 @@ This file creates a simple input stream interface, exported to the root context 
 Created on 6/3/16 by TSPrograms.
 Copyright © 2016 TSPrograms.
 */
+
+
+(function() {
+  var InputStream = function(input) {
+    if (typeof input !== 'string') {
+      throw 'Input to InputStream must be of type string!';
+    }
+    var pos = 0, line = 1, col = 0;
+    this.next = function() {
+        var char = input.charAt(pos++);
+        if (char === "\n") {
+          ++line, col = 0;
+        }
+        else {
+          ++col;
+        }
+        return char;
+    };
+    this.peek = function() {
+        return input.charAt(pos);
+    };
+    this.eof = function() {
+        return peek() === "";
+    };
+    this.croak = function(msg) {
+        throw new Error(msg + " (" + line + ":" + col + ")");
+    }
+  };
+  var root = this; // Get the root context via this in an anonymous function
+  root.InputStream = InputStream;
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = InputStream; // Export to module.exports if module.exports exists
+  }
+});
